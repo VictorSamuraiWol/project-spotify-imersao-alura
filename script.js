36c8e8f40c709f8841487db792e03b193a0c82dd
Victor Cardoso <victorclmatos@gmail.com>
2024-01-31 17:02:20 -0300

    ajustes nas interações, nas cores de fundo, de palavras, mouseover e mouseout, troca de artistas por personagens

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-const resultArtist = document.getElementById('result-artist');
+const resultCharacter = document.getElementById('result-character');
 const playlistContainer = document.getElementById('result-playlists');
 const searchInput = document.getElementById('search-input');
 
+resultCharacter.style.display = 'none';
+
 function requestApi(searchTerm) {
-    fetch(`http://localhost:3000/artists?name_like=${searchTerm}`)
+    fetch(`http://localhost:3000/character?name_like=${searchTerm}`)
       .then((response) => response.json())
       .then((results) => displayResults(results));
   };
 
   function displayResults(results) {
     hidePlaylists();
-    const artistImage = document.getElementById("artist-img");
-    const artistName = document.getElementById("artist-name");
+    const characterImage = document.getElementById("character-img");
+    const characterName = document.getElementById("character-name");
+    const descriptionHero = document.getElementById("descriptionHeroSpan");
 
     results.forEach((element) => {
-      artistImage.src = element.urlImg;
-      artistName.innerText = element.name;
-    });
-    resultArtist.classList.remove("hidden");
-  };
+      characterImage.src = element.urlImg;
+      characterName.innerText = element.name;
+      descriptionHero.innerText = element.description;
+      });
+    resultCharacter.classList.remove("hidden");
+    resultCharacter.style.display = 'flex';
+    };
   
   function hidePlaylists() {
     playlistContainer.classList.add("hidden");
@@ -26,10 +31,20 @@ function requestApi(searchTerm) {
   
   searchInput.addEventListener("input", function () {
     const searchTerm = searchInput.value.toLowerCase();
-    if (searchTerm === "") {
-      resultArtist.classList.add("hidden");
+      if (searchTerm === "") {
+      resultCharacter.classList.add("hidden");
       playlistContainer.classList.remove("hidden");
       return;
     }
     requestApi(searchTerm);
+  });
+
+  resultCharacter.addEventListener('mouseover', function () {
+    const descriptionHero = document.getElementById("descriptionHero");
+    descriptionHero.classList.remove("hidden");
+    descriptionHero.style.display = "flex";
+  });
+
+  resultCharacter.addEventListener('mouseout', function () {
+    descriptionHero.style.display = "none";
   });
