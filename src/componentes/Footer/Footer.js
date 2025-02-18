a7565c12ddef8a42b08e39ec567810ad9f9ee9c2
Victor Cardoso <victorclmatos@gmail.com>
2025-02-17 23:04:57 -0300

    adição do meu contato, add my contact

--- src/componentes/Footer/Footer.js
+++ src/componentes/Footer/Footer.js
@@ -1,4 +1,6 @@
+import { Link } from 'react-router-dom';
 import './Footer.css'
+import wolverine from '../../assets/icons/icone-wolverine-sem-fundo.png'
 
 const Footer = () => {
     return (
@@ -7,6 +9,13 @@ const Footer = () => {
                 <p className="disclaimer-premium__title">Test Premium for free</p>
                 <p className="disclaimer-premium__subtitle">
                     Sign up to enjoy the list of the best superheroes of all time. No credit card required.
+                </p>
+            </div>
+            <div className='contact'>
+                <img src={wolverine} alt="icon wolverine" />
+                <p>Desenvolvido por Victor Cardoso. Entre em contato pelo 
+                    <Link id='contactLink' to="https://www.linkedin.com/in/victor-cardoso-cloud-front/" target="_blank"> Linkedin.
+                    </Link>
                 </p>
             </div>
             <div className="button">
